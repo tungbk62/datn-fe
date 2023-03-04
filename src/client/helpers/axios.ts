8d73ccd69dd9e33ad3dad8a6caec7b14ffd2731e
binh.buidang <dangbinh4869@gmail.com>
2023-03-04 11:15:26 +0700

    add api helper

--- src/client/helpers/axios.ts
+++ src/client/helpers/axios.ts
@@ -1,7 +1,8 @@
 import Axios from "axios";
-import { dispatch, store } from "@app-client/store";
 import Cookies from "js-cookie";
+
 const axios = Axios.create();
+
 setCommonAuthorizationToken();
 
 // Add a request interceptor
@@ -31,14 +32,57 @@ axios.interceptors.response.use(
 
 export default axios;
 
-export async function setCommonAuthorizationToken() {
-  const userToken = await Cookies.get("userToken");
+export function setCommonAuthorizationToken() {
+  const userToken = Cookies.get("userToken");
   if (userToken) {
-    axios.defaults.headers.common.Authorization = "Bearer " + userToken;
+    axios.defaults.headers.common.Authorization = `Bearer ${userToken}`;
   }
 }
 
-export async function removeCommonAuthorizationToken() {
+export function removeCommonAuthorizationToken() {
   delete axios.defaults.headers.common.Authorization;
-  await Cookies.remove("userToken");
+  Cookies.remove("userToken");
 }
+
+export interface ApiResponse<T> {
+  data: T;
+}
+
+export const apiHelper = {
+  get: async function <T>(url: string): Promise<T> {
+    const response = await axios.get<T>(url);
+    return response.data;
+  },
+  post: async function <TRequest, TResponse>(
+    url: string,
+    data: TRequest,
+    config?: any,
+  ): Promise<ApiResponse<TResponse>> {
+    const response = await axios.post<ApiResponse<TResponse>>(
+      url,
+      data,
+      config,
+    );
+    return response.data;
+  },
+  put: async function <TRequest, TResponse>(
+    url: string,
+    data: TRequest,
+  ): Promise<ApiResponse<TResponse>> {
+    const response = await axios.put<ApiResponse<TResponse>>(url, data);
+    return response.data;
+  },
+  patch: async function <TRequest, TResponse>(
+    url: string,
+    data: TRequest,
+  ): Promise<ApiResponse<TResponse>> {
+    const response = await axios.patch<ApiResponse<TResponse>>(url, data);
+    return response.data;
+  },
+  delete: async function <TResponse>(
+    url: string,
+  ): Promise<ApiResponse<TResponse>> {
+    const response = await axios.delete<ApiResponse<TResponse>>(url);
+    return response.data;
+  },
+};
